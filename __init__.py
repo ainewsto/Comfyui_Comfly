2923eb80c3fdfd07beb2d196c9f35adb069ab5ca
ainewsto <113163264+ainewsto@users.noreply.github.com>
2024-07-16 15:11:21 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from . import AiHelper
 
 __all__ = ['AiHelper', 'NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY', 'AiHelperExtension']
 
-
 def start_ai_helper():
     import threading
     import subprocess
@@ -11,14 +10,12 @@ def start_ai_helper():
     import sys
     import aiohttp
 
-
     def run_ai_helper():
         ai_helper_path = os.path.join(os.path.dirname(__file__), "AiHelper.py")
         subprocess.run([sys.executable, ai_helper_path])
 
     ai_helper_thread = threading.Thread(target=run_ai_helper)
     ai_helper_thread.start()
-
 
 start_ai_helper()
 
