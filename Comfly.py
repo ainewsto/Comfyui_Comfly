eb71981f2bf610d581be5d6177608c0f0b45fd12
ainewsto <113163264+ainewsto@users.noreply.github.com>
2025-11-20 20:15:43 +0800

    updated gemini-3-pro-image-preview

--- Comfly.py
+++ Comfly.py
@@ -7582,7 +7582,7 @@ class Comfly_nano_banana:
         return {
             "required": {
                 "text": ("STRING", {"multiline": True}),
-                "model": (["gemini-2.5-flash-image", "nano-banana", "nano-banana-hd", "gemini-2.5-flash-image-preview"], {"default": "nano-banana"}),
+                "model": (["gemini-3-pro-image-preview", "gemini-2.5-flash-image", "nano-banana", "nano-banana-hd", "gemini-2.5-flash-image-preview"], {"default": "nano-banana"}),
             },
             "optional": {
                 "image1": ("IMAGE",),
